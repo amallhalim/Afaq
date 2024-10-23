8b723f7fe5c3c14a0c3e41f19da220f61764d755
Amal_halim <amalhalim333@gmail.com>
2024-10-23 07:34:44 +0300

    install anti design

--- Afaq_web/src/main.jsx
+++ Afaq_web/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ConfigProvider } from 'antd';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
     <App />
+  </ConfigProvider>
   </StrictMode>,
 )
