8249251dbe4ab45f19f44d620aed429dd2e643ff
Amal_halim <amalhalim333@gmail.com>
2024-10-23 10:57:41 +0300

    enhance the code

--- Afaq_web/src/main.jsx
+++ Afaq_web/src/main.jsx
@@ -1,27 +1,26 @@
-import { StrictMode, useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { Cascader, ConfigProvider, Divider, Select, Button } from 'antd';
-import darkTheme from './theme/darkTheme.jsx';
-import lightTheme from './theme/lightTheme.jsx';
-import { Option } from 'antd/es/mentions/index.js';
-import enUS from 'antd/lib/locale/en_US'; 
-import arEG from 'antd/lib/locale/ar_EG'; 
-import './configuration/i18next.js'; // Your i18next config
-import LanguageToggle from './componant/header/LanguageToggle.jsx';
+import {StrictMode, useState} from "react";
+import {createRoot} from "react-dom/client";
+import { Cascader, ConfigProvider, Button } from "antd";
+import enUS from "antd/lib/locale/en_US";
+import arEG from "antd/lib/locale/ar_EG";
 
+import darkTheme from "./theme/darkTheme.jsx";
+import lightTheme from "./theme/lightTheme.jsx";
+import "./configuration/i18next.js";
+
+import App from "./App.jsx";
+import LanguageToggle from "./componant/header/LanguageToggle.jsx";
+
+import "./index.css";
 const Main = () => {
+  
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [langDirection, setLangDirection] = useState("rtl");
   const [currentLang, setCurrentLang] = useState(arEG);
-  console.log("🚀 ~ Main ~ currentLang:", currentLang)
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
-
-
 
   return (
     <StrictMode>
@@ -30,21 +29,19 @@ const Main = () => {
         direction={langDirection}
         theme={isDarkMode ? darkTheme : lightTheme}
       >
-        <div style={{ padding: '20px' }}>
-           <LanguageToggle currentLang={currentLang} setCurrentLang={setCurrentLang} setLangDirection={setLangDirection} />
-          <Button onClick={toggleTheme}>
-            Switch to {isDarkMode ? 'Light' : 'Dark'} Mode
-          </Button>
-
-       
+        <div style={{padding: "20px"}}>
+          <LanguageToggle
+            currentLang={currentLang}
+            setCurrentLang={setCurrentLang}
+            setLangDirection={setLangDirection}
+          />
+          <Button onClick={toggleTheme}>Switch to {isDarkMode ? "Light" : "Dark"} Mode</Button>
         </div>
 
-
-        
         <App />
       </ConfigProvider>
     </StrictMode>
   );
 };
 
-createRoot(document.getElementById('root')).render(<Main />);
+createRoot(document.getElementById("root")).render(<Main />);
